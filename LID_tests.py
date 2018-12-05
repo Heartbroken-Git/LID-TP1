faab4bebb8d879ea13d2bf57e7b1f040b3451f3b
Heartbroken-Git <corentin196@gmail.com>
2018-12-05 17:08:43 +0100

    removed not working whois test

--- LID_tests.py
+++ LID_tests.py
@@ -4,40 +4,12 @@ import LID_sequence_matcher as LseqMatch
 #require pip install whois
 def LIDwhois(domain):
 	_domain = whois.whois(domain)
-	_text = _domain.text
 	_creation = _domain.creation_date
 	_expiration = _domain.expiration_date
 	_domain = _domain.domain_name
 
 
-	return(_domain,_creation,_expiration,_text)
-
-def country(text):
-	info= text.split("country:")[1].split("\n")[0]
-	return(info)
-
-def testWhois(originalDomain, domainToTest):
-	grade = 0
-
-	originalDomainName, originalDomainText, originalDomainCreation, originalDomainExpiration = LIDwhois(originalDomain)
-	originalDomainCountry = country(originalDomainText)
-	testDomainName, testDomainText, testDomainCreation, testDomainExpiration = LIDwhois(domainToTest)
-	testDomainCountry = country(testDomainText)
-
-	if testDomainName != originalDomainName:
-		grade = grade+1
-
-		if testDomainCountry != originalDomainCountry:
-			grade=grade+1
-
-		if testDomainCreation != originalDomainCreation:
-			grade=grade+1
-
-		if testDomainExpiration != originalDomainExpiration:
-			grade=grade+1
-
-	else:
-		return grade
+	return(_domain,_creation,_expiration)
 
 def testBuzzword(urlToTest):
 
@@ -102,9 +74,6 @@ def doTests(urlToTest):
 		dotsGrade = testManyWeirdChar(urlToTest, '.')
 		print("<bold>LID_tests.py > DEBUG : " + urlToTest + " has many dots ? " + str(dotsGrade) + "</bold>")
 
-		print("LID_tests.py > DEBUG : Beginning whois testing")
-		whoisGrade = testWhois(originalDomain, urlToTest)
-		print("<bold>LID_tests.py > DEBUG : " + urlToTest + " graded " + str(whoisGrade) + "</bold>")
 
 	else:
 		print("<red>LID_tests.py > DEBUG : " + urlToTest + " not in scope</red>")
